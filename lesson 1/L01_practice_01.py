2a66395df56f5c590f2ef4bb8c6397ebd4560fa3
dirac <78232235+diracduran@users.noreply.github.com>
2021-11-29 21:20:08 +0300

    Add files via upload

---
+++ lesson 1/L01_practice_01.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Используя функцию print() выведите на экран:
+    1) Ваше имя
+    2) Ваш родной город
+    3) Какой язык Вы изучаете
+"""
+name = 'Dirac'
+hometown = 'Moscow'
+language = 'Python'
+print(name, hometown, language) # Dirac Moscow Python
