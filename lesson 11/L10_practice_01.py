5f309a5394a803413f3a672a82fd3bb66a276bda
dirac <78232235+diracduran@users.noreply.github.com>
2022-02-07 16:09:31 +0300

    Update L10_practice_01.py

--- lesson 11/L10_practice_01.py
+++ lesson 11/L10_practice_01.py
@@ -17,21 +17,25 @@ consonants = 'qwrtpsdfghjklzxcvbnm'
 filename = 'd:\\study_projects\\python_lessons\\lesson 11\\hw_files\\L10_practice_01.json'
 
 def word_stat(some_list, fname):
+    res = {}
     for word in some_list:
         vow_count = 0
         cons_count = 0
+        
         for letter in word.lower():
             if letter in vowels:
                 vow_count += 1
             if letter in consonants:
                 cons_count += 1
-        res = {
-            word: ({
+        
+        res [word] = ({
                 'гласные': vow_count
             }, {
                 'согласные': cons_count
-            })
-        } # {"train": [{"гласные": 2}, {"согласные": 3}]} - ИСПРАВИТЬ!!!
+            }) # {"sea": [{"гласные": 2}, {"согласные": 1}], "belt": [{"гласные": 1}, {"согласные": 3}], "garbage": [{"гласные": 3}, {"согласные": 4}], "river": [{"гласные": 2}, {"согласные": 3}], "house": [{"гласные": 3}, {"согласные": 2}], "school": [{"гласные": 2}, {"согласные": 4}], "boomerang": [{"гласные": 4}, {"согласные": 5}], "constructor": [{"гласные": 3}, {"согласные": 8}], "python": [{"гласные": 2}, {"согласные": 4}], "JavaScript": [{"гласные": 3}, {"согласные": 7}], "train": [{"гласные": 2}, {"согласные": 3}]}
+
+    # print(res)
+    # return res
     
     with open(fname, 'w', encoding='utf-8') as f:
         json.dump(res, f, ensure_ascii=False)
