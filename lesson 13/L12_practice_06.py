1c956e1448f2860848750ff8ae5e12c47cfaa884
dirac <78232235+diracduran@users.noreply.github.com>
2022-03-02 16:22:46 +0300

    Update L12_practice_06.py

--- lesson 13/L12_practice_06.py
+++ lesson 13/L12_practice_06.py
@@ -3,4 +3,15 @@
 """
 1. Создайте классы Horse и Rider. Используйте композицию, чтобы смоделировать лошадь с всадником на ней.
 """
-# Ваш код ниже
+class Horse:
+    def __init__(self, name, age, rider):
+        self.name = name,
+        self.age = age,
+        self.rider = rider
+
+class Rider:
+    def __init__(self, name):
+        self.name = name
+
+dirac = Rider('Dirac')
+horse = Horse('Sunny', 3, dirac)
